f8443d503e31d9072f2b8b96ce21c9b2c858a5c8
Heso113 <hampe_olsson91@hotmail.com>
2020-11-27 11:05:19 +0100

    feature: added input validation to loginForm. Redirects unregistered paths to the NotFound website.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/containers/Login'
 import Dashboard from './components/containers/Dashboard'
 import ProtectedRoute from './routers/protected.route'
+import NotFound from './components/containers/NotFound'
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                 <Switch>
                     <Route path="/login" component={Login} />
                     <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+                    <Route path="*" component={ NotFound } /> 
                 </Switch>
             </div>
         </Router>
--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const LoginForm = props => {
     const [user, setUsername] = useState({});
     const [ validInput, setValidInput ] = useState(false);
-    const [ invalidInputMessage, setInvalidInputMessage ] = useState('Error: Username must contain at least 3 character.');
+    const [ invalidInputMessage, setInvalidInputMessage ] = useState('Please enter a username with atleast 3 characters.');
 
     const onLoginClicked = ev => {
         props.complete(user);
@@ -14,17 +14,17 @@ const LoginForm = props => {
         setUsername(currentInput);
         if (currentInput.length < 3) {
             setValidInput(false);
-            setInvalidInputMessage('Error: Username must contain at least 3 character.');
+            setInvalidInputMessage('Please enter a username with atleast 3 characters.');
         } else if (currentInput.length > 15) {
             setValidInput(false);
-            setInvalidInputMessage('Error: Username can contain a maximum off 15 characters.');
+            setInvalidInputMessage('Username cannot exceed 15 characters in length.');
         } else {
             setValidInput(true);
             for(let i = 0; i < currentInput.length; i++) {
                 let currentChar = currentInput[i];
                 if (!((currentChar >= 'a' && currentChar <= 'z') || (currentChar >= 'A' && currentChar <= 'Z' ))) {
                     setValidInput(false);
-                    setInvalidInputMessage('Error: Sentence may only contain letters.');
+                    setInvalidInputMessage('Error: username may only contain letters.');
                 }
             }
         }
